6bfe6316fb98bf9c26be4cec753a12aeee6e37f2
Prathap <prathap.ufttest@gmail.com>
2020-01-07 12:30:26 +0530

    1 st commit

---
+++ src/project1/c1.java
@@ -0,0 +1,10 @@
+package project1;
+
+public class c1 {
+
+	public static void main(String[] args) {
+		System.out.println("c1 class");
+
+	}
+
+}
---
+++ src/project1/c2.java
@@ -0,0 +1,7 @@
+package project1;
+
+public class c2 {
+	public static void main(String[] args) {
+		System.out.println("c2 class");
+	}
+}
